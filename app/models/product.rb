ad65e3e801e5d8b92d808815872cb037e577568b
dani <dani.zaghian@gmail.com>
2021-05-19 12:12:08 -0700

    adds rails shortcuts and supplier data to product serializer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  belongs_to :supplier
+
+  def supplier_name
+    supplier.name
+  end
+
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: {greater_than: 0}
   validates :description, length: {in: 10..500}
