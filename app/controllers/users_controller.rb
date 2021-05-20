27d5402555b2cc067a15b677d2fcfdefe3bf4ffe
dani <dani.zaghian@gmail.com>
2021-05-20 12:03:09 -0700

    adds signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { id: user.id, name: user.name, email: user.email }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-# suppliers = Supplier.create!([
-#   {name: "Think Geek", email: "thinkgeek@gmail.com", phone_number: "333-4444"},
-#   {name: "Dumbledore's Wizardy Supplies", email: "dws@gmail.com", phone_number: "222-7777"}
-# ])
+suppliers = Supplier.create!([
+  {name: "Think Geek", email: "thinkgeek@gmail.com", phone_number: "333-4444"},
+  {name: "Dumbledore's Wizardy Supplies", email: "dws@gmail.com", phone_number: "222-7777"}
+])
 
-# products = Product.create!([
-#   {name: "WNYX Mug", price: "2.0", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape", quantity: 1, supplier_id: suppliers[1].id},
-#   {name: "Hitchhiker's Guide to the Galaxy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.", quantity: 1, supplier_id: 2},
-#   {name: "Lightsaber", price: "270.0", description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster", quantity: 1, supplier_id: 1},
-#   {name: "Space Cowboy Laser Gun", price: "170.0", description: "This weapon has no other description than, Shiney!", quantity: 1, supplier_id: 1},
-#   {name: "DnD Dice set", price: "57.0", description: "Take down mighty dragons with this timeless set of 20 sided wonders", quantity: 1, supplier_id: 2},
-#   {name: "Sonic Screwdriver", price: "9.0", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood", quantity: 1, supplier_id: 2},
-#   {name: "Yoda sleeping bag", price: "40.0", description: "For real", quantity: 1, supplier_id: 2},
-#   {name: "Hello Kitty Rice Cooker", price: "58.0", description: "Makes sticky rice", quantity: 1, supplier_id: 1}
-# ])
+products = Product.create!([
+  {name: "WNYX Mug", price: "2.0", description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape", quantity: 1, supplier_id: suppliers[1].id},
+  {name: "Hitchhiker's Guide to the Galaxy", price: "42.0", description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover.", quantity: 1, supplier_id: 2},
+  {name: "Lightsaber", price: "270.0", description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster", quantity: 1, supplier_id: 1},
+  {name: "Space Cowboy Laser Gun", price: "170.0", description: "This weapon has no other description than, Shiney!", quantity: 1, supplier_id: 1},
+  {name: "DnD Dice set", price: "57.0", description: "Take down mighty dragons with this timeless set of 20 sided wonders", quantity: 1, supplier_id: 2},
+  {name: "Sonic Screwdriver", price: "9.0", description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood", quantity: 1, supplier_id: 2},
+  {name: "Yoda sleeping bag", price: "40.0", description: "For real", quantity: 1, supplier_id: 2},
+  {name: "Hello Kitty Rice Cooker", price: "58.0", description: "Makes sticky rice", quantity: 1, supplier_id: 1}
+])
 
 Image.create!([
   {url: "https://i.pinimg.com/236x/14/4a/0b/144a0bffe37f29f414e2dae2bd0141cf.jpg", product_id: 1},
