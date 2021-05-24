6c8df5697b44c854664e0659e55b7a4152662287
dani <dani.zaghian@gmail.com>
2021-05-24 13:40:40 -0700

    adds serializer syntax

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :quantity, :is_discounted?, :tax, :total, :supplier_name, :images
+  attributes :id, :name, :price, :description, :quantity
+
+  belongs_to :supplier
+  has_many :images
+  has_many :categories
 end
