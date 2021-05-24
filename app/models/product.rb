3ac3d273e2a0a9860d91d2199ce1a0bbc0bedbc4
dani <dani.zaghian@gmail.com>
2021-05-24 13:12:16 -0700

    adds has many through associations for product and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
   has_many :images #array of image objects
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   def supplier_name
     supplier.name
