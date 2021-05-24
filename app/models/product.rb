d114ab3e57be58aa5bc818f059368251dca19181
dani <dani.zaghian@gmail.com>
2021-05-24 11:44:38 -0700

    adds category and category_product model, associates"

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images #array of image objects
   has_many :orders
+  has_many :category_products
 
   def supplier_name
     supplier.name
